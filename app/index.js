ad35f85f48fa6be27749f7781bb722b28422f131
Ralph Hansell <rhansell@pixelmedia.com>
2013-04-10 16:09:21 -0400

    spaces to tabs to match .editorconfig

--- app/index.js
+++ app/index.js
@@ -7,97 +7,97 @@ var yeoman = require('yeoman-generator');
 
 
 var AppGenerator = module.exports = function Appgenerator(args, options, config) {
-  yeoman.generators.Base.apply(this, arguments);
+	yeoman.generators.Base.apply(this, arguments);
 
-  this.indexFile = this.readFileAsString(path.join(this.sourceRoot(), 'index.html'));
-  this.mainJsFile = '';
+	this.indexFile = this.readFileAsString(path.join(this.sourceRoot(), 'index.html'));
+	this.mainJsFile = '';
 
-  this.on('end', function () {
-    if (options['skip-install']) {
-      console.log('\n\nDone. Just run ' + 'npm install & bower install --dev'.bold.yellow + ' to install the required dependencies.\n\n');
-    } else {
-        console.log('\n\nDone. Running ' + 'npm install & bower install'.bold.yellow + ' for you to install the required dependencies. If this fails, try running the command yourself.\n\n');
-        var cmdExt = ( os.platform() === 'win32' ) ? '.cmd' : '';
-        spawn('npm'+cmdExt, ['install'], { stdio: 'inherit' });
-        spawn('bower'+cmdExt, ['install'], { stdio: 'inherit' });
-    }
-  });
+	this.on('end', function () {
+		if (options['skip-install']) {
+			console.log('\n\nDone. Just run ' + 'npm install & bower install --dev'.bold.yellow + ' to install the required dependencies.\n\n');
+		} else {
+				console.log('\n\nDone. Running ' + 'npm install & bower install'.bold.yellow + ' for you to install the required dependencies. If this fails, try running the command yourself.\n\n');
+				var cmdExt = ( os.platform() === 'win32' ) ? '.cmd' : '';
+				spawn('npm'+cmdExt, ['install'], { stdio: 'inherit' });
+				spawn('bower'+cmdExt, ['install'], { stdio: 'inherit' });
+		}
+	});
 
-  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+	this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
 };
 
 util.inherits(AppGenerator, yeoman.generators.NamedBase);
 
 AppGenerator.prototype.askFor = function askFor() {
-  var cb = this.async();
+	var cb = this.async();
 
-  // welcome message
-  var welcome =
-  '\n    Yo, just so you know...'.yellow.bold+'\n';
+	// welcome message
+	var welcome =
+	'\n    Yo, just so you know...'.yellow.bold+'\n';
 
-  console.log(welcome);
-  console.log('"Out of the box" this includes jQuery and Modernizr.');
+	console.log(welcome);
+	console.log('"Out of the box" this includes jQuery and Modernizr.');
 
-  var prompts = [{
-    name: 'compassBootstrap',
-    message: 'Would you like to include Twitter Bootstrap for Sass?',
-    default: 'Y/n',
-    warning: 'Yes: All Twitter Bootstrap files will be placed into the styles directory.'
-  }
-  // , {}
-  ];
+	var prompts = [{
+		name: 'compassBootstrap',
+		message: 'Would you like to include Twitter Bootstrap for Sass?',
+		default: 'Y/n',
+		warning: 'Yes: All Twitter Bootstrap files will be placed into the styles directory.'
+	}
+	// , {}
+	];
 
-  this.prompt(prompts, function (err, props) {
-    if (err) {
-      return this.emit('error', err);
-    }
+	this.prompt(prompts, function (err, props) {
+		if (err) {
+			return this.emit('error', err);
+		}
 
-    // manually deal with the response, get back and store the results.
-    // we change a bit this way of doing to automatically do this in the self.prompt() method.
-    this.compassBootstrap = (/y/i).test(props.compassBootstrap);
-    this.includeRequireJS = (/y/i).test(props.includeRequireJS);
+		// manually deal with the response, get back and store the results.
+		// we change a bit this way of doing to automatically do this in the self.prompt() method.
+		this.compassBootstrap = (/y/i).test(props.compassBootstrap);
+		this.includeRequireJS = (/y/i).test(props.includeRequireJS);
 
-    cb();
-  }.bind(this));
+		cb();
+	}.bind(this));
 };
 
 AppGenerator.prototype.gruntfile = function gruntfile() {
-  this.template('Gruntfile.js');
+	this.template('Gruntfile.js');
 };
 
 AppGenerator.prototype.packageJSON = function packageJSON() {
-  this.template('_package.json', 'package.json');
+	this.template('_package.json', 'package.json');
 };
 
 AppGenerator.prototype.git = function git() {
-  this.copy('gitignore', '.gitignore');
-  this.copy('gitattributes', '.gitattributes');
+	this.copy('gitignore', '.gitignore');
+	this.copy('gitattributes', '.gitattributes');
 };
 
 AppGenerator.prototype.bower = function bower() {
-  this.copy('bowerrc', '.bowerrc');
-  this.copy('_component.json', 'component.json');
+	this.copy('bowerrc', '.bowerrc');
+	this.copy('_component.json', 'component.json');
 };
 
 AppGenerator.prototype.editorConfig = function editorConfig() {
-  this.copy('editorconfig', '.editorconfig');
+	this.copy('editorconfig', '.editorconfig');
 };
 
 AppGenerator.prototype.misc = function misc() {
-  this.copy('favicon.ico', 'app/favicon.ico');
-  this.copy('404.html', 'app/404.html');
-  this.copy('robots.txt', 'app/robots.txt');
+	this.copy('favicon.ico', 'app/favicon.ico');
+	this.copy('404.html', 'app/404.html');
+	this.copy('robots.txt', 'app/robots.txt');
 };
 
 AppGenerator.prototype.app = function app() {
-  this.mkdir('app');
-  this.mkdir('app/scripts');
-  this.mkdir('app/styles');
-  this.mkdir('app/images');
-  this.write('app/index.html', this.indexFile);
-  this.write('app/scripts/main.js', this.mainJsFile);
+	this.mkdir('app');
+	this.mkdir('app/scripts');
+	this.mkdir('app/styles');
+	this.mkdir('app/images');
+	this.write('app/index.html', this.indexFile);
+	this.write('app/scripts/main.js', this.mainJsFile);
 };
 
 AppGenerator.prototype.scss = function scss() {
-  this.write('app/styles/screen.scss', '//screen.scss');
+	this.write('app/styles/screen.scss', '//screen.scss');
 };
--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -2,233 +2,233 @@
 'use strict';
 var lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet;
 var mountFolder = function (connect, dir) {
-    return connect.static(require('path').resolve(dir));
+	return connect.static(require('path').resolve(dir));
 };
 
 module.exports = function (grunt) {
-    // load all grunt tasks
-    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+	// load all grunt tasks
+	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-    // configurable paths
-    var yeomanConfig = {
-        app: 'app',
-        dist: 'dist'
-    };
+	// configurable paths
+	var yeomanConfig = {
+		app: 'app',
+		dist: 'dist'
+	};
 
-    grunt.initConfig({
-        yeoman: yeomanConfig,
-        watch: {
-            compass: {
-                files: ['<%%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
-                tasks: ['compass:server']
-            },
-            livereload: {
-                files: [
-                    '<%%= yeoman.app %>/*.html',
-                    '{.tmp,<%%= yeoman.app %>}/styles/{,*/}*.css',
-                    '{.tmp,<%%= yeoman.app %>}/scripts/{,*/}*.js',
-                    '<%%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
-                ],
-                tasks: ['livereload']
-            }
-        },
-        connect: {
-            options: {
-                port: 9000,
-                // change this to '0.0.0.0' to access the server from outside
-                hostname: 'localhost'
-            },
-            livereload: {
-                options: {
-                    middleware: function (connect) {
-                        return [
-                            lrSnippet,
-                            mountFolder(connect, '.tmp'),
-                            mountFolder(connect, 'app')
-                        ];
-                    }
-                }
-            },
-            dist: {
-                options: {
-                    middleware: function (connect) {
-                        return [
-                            mountFolder(connect, 'dist')
-                        ];
-                    }
-                }
-            }
-        },
-        open: {
-            server: {
-                path: 'http://localhost:<%%= connect.options.port %>'
-            }
-        },
-        clean: {
-            dist: {
-                files: [{
-                    dot: true,
-                    src: [
-                        '.tmp',
-                        '<%%= yeoman.dist %>/*',
-                        '!<%%= yeoman.dist %>/.git*'
-                    ]
-                }]
-            },
-            server: '.tmp'
-        },
-        jshint: {
-            options: {
-                jshintrc: '.jshintrc'
-            },
-            all: [
-                'Gruntfile.js',
-                '<%%= yeoman.app %>/scripts/{,*/}*.js',
-                '!<%%= yeoman.app %>/scripts/vendor/*'
-            ]
-        },
-        compass: {
-            options: {
-                sassDir: '<%%= yeoman.app %>/styles',
-                cssDir: '.tmp/styles',
-                imagesDir: '<%%= yeoman.app %>/images',
-                javascriptsDir: '<%%= yeoman.app %>/scripts',
-                fontsDir: '<%%= yeoman.app %>/styles/fonts',
-                importPath: 'app/components',
-                relativeAssets: true
-            },
-            dist: {},
-            server: {
-                options: {
-                    debugInfo: true
-                }
-            }
-        },
-        <% if (includeRequireJS) { %>
-        requirejs: {
-            dist: {
-                // Options: https://github.com/jrburke/r.js/blob/master/build/example.build.js
-                options: {
-                    // `name` and `out` is set by grunt-usemin
-                    baseUrl: 'app/scripts',
-                    optimize: 'none',
-                    // TODO: Figure out how to make sourcemaps work with grunt-usemin
-                    // https://github.com/yeoman/grunt-usemin/issues/30
-                    //generateSourceMaps: true,
-                    // required to support SourceMaps
-                    // http://requirejs.org/docs/errors.html#sourcemapcomments
-                    preserveLicenseComments: false,
-                    useStrict: true,
-                    wrap: true
-                }
-            }
-        },
-        <% } %>
-        rev: {
-            dist: {
-                files: {
-                    src: [
-                        '<%%= yeoman.dist %>/scripts/{,*/}*.js',
-                        '<%%= yeoman.dist %>/styles/{,*/}*.css',
-                        '<%%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp}',
-                        '<%%= yeoman.dist %>/styles/fonts/*'
-                    ]
-                }
-            }
-        },
-        imagemin: {
-            dist: {
-                files: [{
-                    expand: true,
-                    cwd: '<%%= yeoman.app %>/images',
-                    src: '{,*/}*.{png,jpg,jpeg}',
-                    dest: '<%%= yeoman.dist %>/images'
-                }]
-            }
-        },
-        svgmin: {
-            dist: {
-                files: [{
-                    expand: true,
-                    cwd: '<%%= yeoman.app %>/images',
-                    src: '{,*/}*.svg',
-                    dest: '<%%= yeoman.dist %>/images'
-                }]
-            }
-        },
-        cssmin: {
-            dist: {
-                files: {
-                    '<%%= yeoman.dist %>/styles/main.css': [
-                        '.tmp/styles/{,*/}*.css',
-                        '<%%= yeoman.app %>/styles/{,*/}*.css'
-                    ]
-                }
-            }
-        },
-        // Put files not handled in other tasks here
-        copy: {
-            dist: {
-                files: [{
-                    expand: true,
-                    dot: true,
-                    cwd: '<%%= yeoman.app %>',
-                    dest: '<%%= yeoman.dist %>',
-                    src: [
-                        '*.{ico,txt}',
-                        '.htaccess',
-                        'images/{,*/}*.{webp,gif}',
-                        'styles/fonts/*'
-                    ]
-                }]
-            }
-        },
-        concurrent: {
-            server: [
-                'compass:server'
-            ],
-            dist: [
-                'compass:dist',
-                'imagemin',
-                'svgmin'
-            ]
-        }<% if (includeRequireJS) { %>,
-        bower: {
-            options: {
-                exclude: ['modernizr']
-            },
-            all: {
-                rjsConfig: '<%%= yeoman.app %>/scripts/main.js'
-            }
-        }<% } %>
-    });
+	grunt.initConfig({
+		yeoman: yeomanConfig,
+		watch: {
+			compass: {
+				files: ['<%%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
+				tasks: ['compass:server']
+			},
+			livereload: {
+				files: [
+					'<%%= yeoman.app %>/*.html',
+					'{.tmp,<%%= yeoman.app %>}/styles/{,*/}*.css',
+					'{.tmp,<%%= yeoman.app %>}/scripts/{,*/}*.js',
+					'<%%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+				],
+				tasks: ['livereload']
+			}
+		},
+		connect: {
+			options: {
+				port: 9000,
+				// change this to '0.0.0.0' to access the server from outside
+				hostname: 'localhost'
+			},
+			livereload: {
+				options: {
+					middleware: function (connect) {
+						return [
+							lrSnippet,
+							mountFolder(connect, '.tmp'),
+							mountFolder(connect, 'app')
+						];
+					}
+				}
+			},
+			dist: {
+				options: {
+					middleware: function (connect) {
+						return [
+							mountFolder(connect, 'dist')
+						];
+					}
+				}
+			}
+		},
+		open: {
+			server: {
+				path: 'http://localhost:<%%= connect.options.port %>'
+			}
+		},
+		clean: {
+			dist: {
+				files: [{
+					dot: true,
+					src: [
+						'.tmp',
+						'<%%= yeoman.dist %>/*',
+						'!<%%= yeoman.dist %>/.git*'
+					]
+				}]
+			},
+			server: '.tmp'
+		},
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			all: [
+				'Gruntfile.js',
+				'<%%= yeoman.app %>/scripts/{,*/}*.js',
+				'!<%%= yeoman.app %>/scripts/vendor/*'
+			]
+		},
+		compass: {
+			options: {
+				sassDir: '<%%= yeoman.app %>/styles',
+				cssDir: '.tmp/styles',
+				imagesDir: '<%%= yeoman.app %>/images',
+				javascriptsDir: '<%%= yeoman.app %>/scripts',
+				fontsDir: '<%%= yeoman.app %>/styles/fonts',
+				importPath: 'app/components',
+				relativeAssets: true
+			},
+			dist: {},
+			server: {
+				options: {
+					debugInfo: true
+				}
+			}
+		},
+		<% if (includeRequireJS) { %>
+		requirejs: {
+			dist: {
+				// Options: https://github.com/jrburke/r.js/blob/master/build/example.build.js
+				options: {
+					// `name` and `out` is set by grunt-usemin
+					baseUrl: 'app/scripts',
+					optimize: 'none',
+					// TODO: Figure out how to make sourcemaps work with grunt-usemin
+					// https://github.com/yeoman/grunt-usemin/issues/30
+					//generateSourceMaps: true,
+					// required to support SourceMaps
+					// http://requirejs.org/docs/errors.html#sourcemapcomments
+					preserveLicenseComments: false,
+					useStrict: true,
+					wrap: true
+				}
+			}
+		},
+		<% } %>
+		rev: {
+			dist: {
+				files: {
+					src: [
+						'<%%= yeoman.dist %>/scripts/{,*/}*.js',
+						'<%%= yeoman.dist %>/styles/{,*/}*.css',
+						'<%%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp}',
+						'<%%= yeoman.dist %>/styles/fonts/*'
+					]
+				}
+			}
+		},
+		imagemin: {
+			dist: {
+				files: [{
+					expand: true,
+					cwd: '<%%= yeoman.app %>/images',
+					src: '{,*/}*.{png,jpg,jpeg}',
+					dest: '<%%= yeoman.dist %>/images'
+				}]
+			}
+		},
+		svgmin: {
+			dist: {
+				files: [{
+					expand: true,
+					cwd: '<%%= yeoman.app %>/images',
+					src: '{,*/}*.svg',
+					dest: '<%%= yeoman.dist %>/images'
+				}]
+			}
+		},
+		cssmin: {
+			dist: {
+				files: {
+					'<%%= yeoman.dist %>/styles/main.css': [
+						'.tmp/styles/{,*/}*.css',
+						'<%%= yeoman.app %>/styles/{,*/}*.css'
+					]
+				}
+			}
+		},
+		// Put files not handled in other tasks here
+		copy: {
+			dist: {
+				files: [{
+					expand: true,
+					dot: true,
+					cwd: '<%%= yeoman.app %>',
+					dest: '<%%= yeoman.dist %>',
+					src: [
+						'*.{ico,txt}',
+						'.htaccess',
+						'images/{,*/}*.{webp,gif}',
+						'styles/fonts/*'
+					]
+				}]
+			}
+		},
+		concurrent: {
+			server: [
+				'compass:server'
+			],
+			dist: [
+				'compass:dist',
+				'imagemin',
+				'svgmin'
+			]
+		}<% if (includeRequireJS) { %>,
+		bower: {
+			options: {
+				exclude: ['modernizr']
+			},
+			all: {
+				rjsConfig: '<%%= yeoman.app %>/scripts/main.js'
+			}
+		}<% } %>
+	});
 
-    grunt.renameTask('regarde', 'watch');
+	grunt.renameTask('regarde', 'watch');
 
-    grunt.registerTask('server', function (target) {
-        if (target === 'dist') {
-            return grunt.task.run(['build', 'open', 'connect:dist:keepalive']);
-        }
+	grunt.registerTask('server', function (target) {
+		if (target === 'dist') {
+			return grunt.task.run(['build', 'open', 'connect:dist:keepalive']);
+		}
 
-        grunt.task.run([
-            'clean:server',
-            'concurrent:server',
-            'livereload-start',
-            'connect:livereload',
-            'open',
-            'watch'
-        ]);
-    });
+		grunt.task.run([
+			'clean:server',
+			'concurrent:server',
+			'livereload-start',
+			'connect:livereload',
+			'open',
+			'watch'
+		]);
+	});
 
-    grunt.registerTask('build', [
-        'clean:dist',
-        'concurrent:dist',<% if (includeRequireJS) { %>
-        'requirejs',<% } %>
-        'copy',
-        'rev'
-    ]);
+	grunt.registerTask('build', [
+		'clean:dist',
+		'concurrent:dist',<% if (includeRequireJS) { %>
+		'requirejs',<% } %>
+		'copy',
+		'rev'
+	]);
 
-    grunt.registerTask('default', [
-        'build'
-    ]);
+	grunt.registerTask('default', [
+		'build'
+	]);
 };
